e0e779d03f71b744fe0dec85801d483697ff28ed
Maciej Sobolewski <macieksob25@gmail.com>
2021-03-20 14:59:56 +0100

    Caly proejkt

---
+++ AddressBook1/Pages/Index.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using AddressBook.Models;
+
+namespace AddressBook.Pages
+
+{
+    public class IndexModel : PageModel
+    {
+        public Address Address { get; set; }
+        private readonly ILogger<IndexModel> _logger;
+        [BindProperty(SupportsGet = true)]
+        public string Name { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+                Name = "User";
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            return RedirectToPage("./Privacy");
+        }
+    }
+}
